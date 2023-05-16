7ceaf0fdfacbbfa6bd4d302c6391987e4784490d
marcosm <marcos.martilotta@gmail.com>
2023-05-16 11:52:57 -0300

    maquetado del panel principal

--- client/src/pages/Home/panelIngreso/PanelIngreso.jsx
+++ client/src/pages/Home/panelIngreso/PanelIngreso.jsx
@@ -1,7 +1,25 @@
 import "./panelIngreso.css";
 
 const PanelIngreso = () => {
-	return <div className="panelIngreso"> Panel</div>;
+	return (
+		<div className="panelIngreso">
+			<div className="top-buttons-container">
+				<button className="top-button">Pide tu turno</button>
+				<button className="top-button">consulta tu turno</button>
+			</div>
+			<hr />
+			<div className="bottom-section">
+				<h3>Optimiza tu tiempo y solicita un turno aquí</h3>
+				<p>Pide un turno para ser atendido en tu farmacia</p>
+				<label htmlFor="dni">Número de identidad</label>
+				<input
+					type="number"
+					id="dni"
+				/>
+				<button>INGRESAR</button>
+			</div>
+		</div>
+	);
 };
 
 export default PanelIngreso;
