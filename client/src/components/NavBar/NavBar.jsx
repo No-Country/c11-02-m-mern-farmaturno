fcdcc9d92601c33d52120dd8806123cf5c5353f7
NozoDev <kevinroj326@gmail.com>
2023-05-16 17:12:55 -0500

    navbar y agg el link de bootstrap icon

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,5 +1,22 @@
+import './navbar.css';
 const NavBar = () => {
-	return <nav>navBar</nav>;
+	return <nav class="navbar">
+	<div class="navbar-brand">
+	  <img src="/Farmaturno.jpg" alt="" />
+	</div>
+	<div class="navbar-section">
+	  <a class="navbar-link" href="#">
+		A cerca de FarmaTurno
+		<span class="navbar-icon"></span>
+		<i class="bi bi-chevron-down"></i>
+		</a>
+	</div>
+	<div class="nav-support">
+	  <h3>Soy Farmacia</h3>
+	  <a href="#">Ayuda</a>
+	
+	</div>
+  </nav>
 };
 
 export default NavBar;
