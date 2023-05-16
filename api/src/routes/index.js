c4002b215af3551b137b32b4b0fa9e574c067e43
Diego G <diegoar.guz@hotmail.com>
2023-05-16 08:56:29 -0500

    fixed route customer and deploy

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,10 @@
+
+const { redirectUrl } = require('../utils/redirectUrl');
 const customer = require('./customer.routes');
 
 const routes = (app) => {
-
   app.use('/api/customer', customer);
+  app.get('/', redirectUrl);
 }
 
 
