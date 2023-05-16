44bd0a57b5284646a6b103c4c7fa7023980ab767
Diego G <diegoar.guz@hotmail.com>
2023-05-16 11:55:53 -0500

    Turn controller

---
+++ api/src/controllers/turns.controller.js
@@ -0,0 +1,10 @@
+const { handleHttpError } = require("../utils/handleError");
+const { matchedData } = require("express-validator");
+const { customerModel } = require("../models");
+
+/**
+ * Get turns details
+ * @param {*} req
+ * @param {*} res
+ */
+
