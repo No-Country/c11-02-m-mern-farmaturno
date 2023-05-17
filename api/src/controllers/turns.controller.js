3f43c285772fd200d401c48ba6876ce6d3724e8f
Diego G <diegoar.guz@hotmail.com>
2023-05-17 17:20:14 -0500

    Rutas y Controladores de Turnos

--- api/src/controllers/turns.controller.js
+++ api/src/controllers/turns.controller.js
@@ -1,10 +1,71 @@
 const { handleHttpError } = require("../utils/handleError");
 const { matchedData } = require("express-validator");
-const { customerModel } = require("../models");
+const { turnModel } = require("../models");
 
 /**
- * Get turns details
+ * Get Turn by id
+ * @param {*} req
+ * @param {*} res
+*/
+const getTurn = async (req, res) => {
+    const id = req.params.id.toString();
+    const finded= await turnModel.findById(id);
+    try {
+      res.status(200).send(finded);
+      
+    } catch (error) {
+      handleHttpError(res, "Internal Server Error", 500, "getTurn", error);
+    }
+};
+
+/**
+ * Get Turns from DataBase
+ * @param {*} req
+ * @param {*} res
+*/
+const getAllTurns = async (req, res) => {
+  const turns = await turnModel.find({});
+    try {
+      res.status(200).send(turns);
+    } catch (error) {
+      handleHttpError(res, "Internal Server Error", 500, "getAllTurns", error);
+    }
+};
+
+/**
+ * Create Turn in DataBase
+ * @param {*} req
+ * @param {*} res
+*/
+const createTurn = async (req, res) => {
+    const body=req.body;
+    await turnModel.create(body);
+    try {
+      res.status(200).send("Turn created");
+    } catch (error) {
+      handleHttpError(res, "Internal Server Error", 400, "createTurn", error);
+    }
+};
+
+/**
+ * Modify Turn-status in DataBase
  * @param {*} req
  * @param {*} res
  */
+const modifyTurn = async (req, res) => {
+   const id = req.params.id.toString();
+   const modify= await turnModel.updateOne({ _id: id }, {status: false });
+   try {
+     const finded= await turnModel.findById(id);
+      res.status(200).send("Turno modificado: "+finded);
+   } catch (error) {
+      handleHttpError(res, "Internal Server Error", 500, "modifyTurn", error);
+    }
+  };
 
+  module.exports = {
+    getTurn,
+    getAllTurns,
+    createTurn,
+    modifyTurn
+  };
