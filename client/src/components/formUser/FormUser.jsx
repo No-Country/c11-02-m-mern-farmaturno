0ef4bfe15b5fe72a221ed5bd2efd05fb59134cd0
Victoria <totii.teran@gmail.com>
2023-05-17 11:03:23 -0300

    doy estilo al form

--- client/src/components/formUser/FormUser.jsx
+++ client/src/components/formUser/FormUser.jsx
@@ -5,63 +5,56 @@ import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import Modal from "react-bootstrap/Modal";
-import Stack from 'react-bootstrap/Stack';
-import Ratio from 'react-bootstrap/Ratio';
+import Stack from "react-bootstrap/Stack";
+import Ratio from "react-bootstrap/Ratio";
+import "./FormUserStyle.css";
 
 const FormUser = () => {
- 
-
   return (
     <>
-    <Ratio aspectRatio="16x9">
-    <div style={{ width: 660, height: 'auto' }}>
+      <Container className="container">
+        <Form >
+          <h1>Farmacia Cruz Verde</h1>
+          <h2 className="mb-3">/Direccion/Horario de atencion</h2>
+          <Row className="justify-content-md-center">
+            <Form.Group className="mb-3" as={Col}  controlId="formGridName">
+              <Form.Label>Nombre</Form.Label>
+              <Form.Control className="form" type="name" placeholder="Ingrese su nombre" />
+            </Form.Group>
 
-    <Form>
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridName">
-            <Form.Label>Nombre</Form.Label>
-            <Form.Control type="name" placeholder="Ingrese su nombre" />
-          </Form.Group>
+            <Form.Group  className="mb-3"as={Col}  controlId="formGridLastname">
+              <Form.Label>Apellido</Form.Label>
+              <Form.Control className="form" type="name" placeholder="Ingrese su Apellido" />
+            </Form.Group>
+          </Row>
+          <br />
+          <Row className="justify-content-md-center">
+            <Form.Group className="mb-3" as={Col}  controlId="formGridNumber">
+              <Form.Label>Número telefónico</Form.Label>
+              <Form.Control
+              className="form"
+                type="number"
+                placeholder="Ingrese su número telefónico"
+              />
+            </Form.Group>
 
-          <Form.Group as={Col} controlId="formGridLastname">
-            <Form.Label>Apellido</Form.Label>
-            <Form.Control type="name" placeholder="Ingrese su Apellido" />
-          </Form.Group>
-        </Row>
+            <Form.Group className="mb-3" as={Col} xs={4} controlId="formGridState">
+              <Form.Label>Tu turno</Form.Label>
+              <Form.Select className="form" defaultValue="Choose...">
+                <option>Seleccionar turno...</option>
+                <option>...</option>
+              </Form.Select>
+            </Form.Group>
+          </Row>
+          <br />
 
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridNumber">
-            <Form.Label>Número telefónico</Form.Label>
-            <Form.Control
-              type="number"
-              placeholder="Ingrese su número telefónico"
-            />
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridState">
-          <Form.Label>Tu turno</Form.Label>
-          <Form.Select defaultValue="Choose...">
-            <option>Choose...</option>
-            <option>...</option>
-          </Form.Select>
-        </Form.Group>
-
-        </Row>
-
-        <Stack gap={2} className="col-md-5 mx-auto">
-        <Button variant="secondary" type="submit" >
-          Pedir turno
-        </Button>
-    </Stack>
-
-        
-      </Form>
-      </div>
-        
-      </Ratio>
-      
-
-      
+          <Stack gap={2} className="col-md-5 mx-auto">
+            <Button className="button" variant="secondary" type="submit">
+              PEDIR TURNO
+            </Button>
+          </Stack>
+        </Form>
+      </Container>
     </>
   );
 };
