a14c230865d31e12160aa4fc7699b22b6a4f86dc
Victor Rueda <vialruma1985@gmail.com>
2023-05-19 11:41:00 -0500

    bug validators customer in controller turn

--- api/src/routes/turn.routes.js
+++ api/src/routes/turn.routes.js
@@ -1,6 +1,7 @@
 const {Router} = require('express');
 const router = Router();
 const {getTurn, getAllTurns, createTurn, modifyTurn} = require("../controllers/turns.controller");
+const {validatorCreateCustomer} = require('../validators')
 
 /**
  * Get Turn details
@@ -15,7 +16,7 @@ router.get('/', getAllTurns);
 /**
  * Create Turn in DB
  */
-router.post('/', createTurn);
+router.post('/',validatorCreateCustomer, createTurn);
 
 /**
  * modify Turn-status in DB
