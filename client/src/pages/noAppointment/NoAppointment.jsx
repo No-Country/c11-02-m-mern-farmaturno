3e76b84aced3be17893cc0847b94215b6e4ad276
martinasorialanzi <martinasorialanzi@gmail.com>
2023-05-19 13:15:16 -0300

    estilos page noAppointment

--- client/src/pages/noAppointment/NoAppointment.jsx
+++ client/src/pages/noAppointment/NoAppointment.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import '../noAppointment/noAppointment.css';
+import { Image,Button } from 'react-bootstrap';
 
 const NoAppointment = () => {
-  return (
-    <div>NoAppointment</div>
-  )
-}
+    return (
+      <div className="container">
+        <div className='p-0'>
 
-export default NoAppointment
+        <img
+          src="https://i.ibb.co/ZKH9Fxh/no-Appointment.png"
+          alt="No Appointment"
+          className="imgNoAppointment"
+          />
+          </div>
+  
+        <div className="textContainer">
+          <p className="title">¡Lo sentimos!</p>
+          <p className="description">
+            En estos momentos no contamos con turnos disponibles. Por favor, espera
+            a que la farmacia habilite los turnos e inténtelo de nuevo más tarde.
+          </p>
+        </div>
+        <div className='divBtn'>
+            <Button className='BtnVolverInicio'>Volver al inicio</Button>
+        </div>
+      </div>
+    );
+  };
+
+export default NoAppointment;
