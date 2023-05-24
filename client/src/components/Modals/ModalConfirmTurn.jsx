4ed8cc205197fb2e83a336f5cb295f1232789d7a
Jakiro12 <eclipsisnocte@gmail.com>
2023-05-24 01:55:15 -0300

    modal sin avisos especificos aun

--- client/src/components/Modals/ModalConfirmTurn.jsx
+++ client/src/components/Modals/ModalConfirmTurn.jsx
@@ -1,9 +1,14 @@
-import './styleConfirm.css'
+import './styleConfirm.css';
+import logo from '../../images/success.png';
+import { useNavigate } from 'react-router-dom';
 export default function ModalToConfirmYourTurn ({closeMenu}){
+    const navigate=useNavigate()
     return(
         <div className='container_modal_confirm'>
            <article>
-           <header className='alert_display'><div className='logo'>G</div>
+           <header className='alert_display'><div className='logo'>
+            <img src={logo} className='icon_alert' alt='icono de logrado' />
+           </div>
            <button onClick={closeMenu}>X</button></header>
            <h3 className='message_alert'>Turno confirmado!</h3>
            <div className='data_turn'>
@@ -16,7 +21,7 @@ Recuerda que puedes dar de baja el turno solicitado desde la sección de “Cons
            </div>
            <footer className='btn_container'>
             <button className='check'>Consultar turno</button>
-            <button className='came_back'>Volver al inicio</button>
+            <button className='came_back' onClick={()=>navigate('/')}>Volver al inicio</button>
            </footer>
             </article>
         </div>
