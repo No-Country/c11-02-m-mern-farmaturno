29fb9b05dbc0e304561126bdf3b6a35bbbc4d900
marcosm <marcos.martilotta@gmail.com>
2023-05-27 12:16:06 -0300

    Navegacion entre los formularios y progressBar

--- client/src/pages/signUpPharmacy/SignUpPharmacy.jsx
+++ client/src/pages/signUpPharmacy/SignUpPharmacy.jsx
@@ -1,19 +1,33 @@
-import { Button } from 'react-bootstrap';
-import ProgressBar from 'react-bootstrap/ProgressBar';
+import { ProgressBar } from 'react-bootstrap';
+import PharmacyOwnerDetails from './pharmacyOwnerDetails/PharmacyOwnerDetails';
+import PharmacyDetails from './pharmacyDetails/PharmacyDetails';
+import SignInDetails from './signInDetails/SignInDetails';
 import './signUpPharmacy.css';
+import { useContext } from 'react';
+import { SignUpContext } from './context/pharmacyContext';
+import { useEffect } from 'react';
 import { useState } from 'react';
-import FistFormToComplete from './firstDataForm/firstForm';
+
 const SignUpPharmacy = () => {
-  const [seeFormRequired,setSeeFormRequired] = useState('')
+  const { formToShow } = useContext(SignUpContext);
+  const [now, setNow] = useState(0);
+
+  console.log(now);
+  useEffect(() => {
+    if (formToShow === 'b') {
+      setNow(40);
+    } else if (formToShow === 'c') {
+      setNow(80);
+    }
+  }, [formToShow]);
+
   return (
     <div className="signUpPharmacy">
-      <section className="details">
-        <p>titulo mas boton</p>
-        {
-          seeFormRequired === '' ? <FistFormToComplete changeMenu={()=>setSeeFormRequired('a')}/> : <p>chau</p>
-        }
-      </section>
-      <ProgressBar variant="success" now={30} />
+      {formToShow === 'a' && <PharmacyOwnerDetails />}
+      {formToShow === 'b' && <PharmacyDetails />}
+      {formToShow === 'c' && <SignInDetails />}
+
+      <ProgressBar now={now} variant="success" />
     </div>
   );
 };
--- client/src/pages/signUpPharmacy/pharmacyOwnerDetails/PharmacyOwnerDetails.jsx
+++ client/src/pages/signUpPharmacy/pharmacyOwnerDetails/PharmacyOwnerDetails.jsx
@@ -1,5 +1,19 @@
+import { Button } from 'react-bootstrap';
+import { useContext } from 'react';
+import { SignUpContext } from '../context/pharmacyContext';
+
 const PharmacyOwnerDetails = () => {
-  return <div></div>;
+  const { setFormToShow } = useContext(SignUpContext);
+
+  const handleClick = () => {
+    setFormToShow('b');
+  };
+  return (
+    <div>
+      Owner
+      <Button onClick={handleClick}>Siguiente</Button>
+    </div>
+  );
 };
 
 export default PharmacyOwnerDetails;
