a682f9109fe43849c493c0d17f48cda603b83b51
Victoria <totii.teran@gmail.com>
2023-05-30 10:14:27 -0300

    modal de consultar turno en el panel de ingreso

--- client/src/pages/PageNewAppointment/PageNewAppointment.jsx
+++ client/src/pages/PageNewAppointment/PageNewAppointment.jsx
@@ -1,5 +1,6 @@
 import FormUser from '../../components/formUser/FormUser';
 import moment from 'moment';
+import '../PageNewAppointment/PageNewAppointment.css'
 
 const PageNuevoTurno = () => {
   const currentDate = moment().format(' D/MM/YYYY');
@@ -7,13 +8,15 @@ const PageNuevoTurno = () => {
   return (
     <div className='general'>
       <div>
-      <h1>Pide tu turno</h1>
+       <h1>Pide tu turno</h1>
       <h2>
         Recuerda que estas reservando un turno para el dia de hoy {currentDate}
       </h2>
-      <img src="https://i.ibb.co/VJwHCqN/pana.png" alt="pana" border="0" />
       </div>
+    <div className='body'>
+     <img className='img' src="https://i.ibb.co/VJwHCqN/pana.png" alt="pana" border="0" />
       <FormUser />
+    </div>
     </div>
   );
 };
