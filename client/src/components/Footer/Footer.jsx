41196c531db4897e493cf19cd23b4773b87f809f
martinasorialanzi <martinasorialanzi@gmail.com>
2023-06-06 02:19:11 -0300

    links footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -13,12 +13,12 @@ const Footer = () => {
         </p>
         <Row lg={3} className="footer-links justify-content-center">
           <Col xs={12}>
-            <a href="/privacidad" className="footer-link p-0">
+            <a href="https://drive.google.com/file/d/1LxTcS5IrkKUMO1f0uNqHt8xra3-TplGK/view" target='_blank' className="footer-link p-0">
               Políticas de privacidad
             </a>
           </Col>
           <Col xs={12}>
-            <a href="/terminos" className="footer-link p-0">
+            <a href="https://drive.google.com/file/d/1FiGncNfCX7mb2QH-fDWp6-VFUQfnUgSo/view" target='_blank' className="footer-link p-0">
               Términos y condiciones
             </a>
           </Col>
