6ae5daa23d0018714426902e2351346e327c8548
martinasorialanzi <martinasorialanzi@gmail.com>
2023-06-09 00:24:20 -0300

    correccion diseño footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,31 +3,27 @@ import '../Footer/footer.css';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-container">
-        <div className="paragraph-footer">
-          <p className="footer-brand">
-            © 2023 <span className="brand-color">FarmaTurno</span>. Todos los
-            derechos reservados
-          </p>
-        </div>
-        <Row lg={3} className="footer-links justify-content-center">
-          <a
-            href="https://drive.google.com/file/d/1LxTcS5IrkKUMO1f0uNqHt8xra3-TplGK/view"
-            target="_blank"
-            className="footer-link p-0"
-          >
-            Políticas de privacidad
-          </a>
-          <a
-            href="https://drive.google.com/file/d/1FiGncNfCX7mb2QH-fDWp6-VFUQfnUgSo/view"
-            target="_blank"
-            className="footer-link p-0"
-          >
-            Términos y condiciones
-          </a>
+    <footer className="footer1">
+      <Row className="footer-container1">
+        <p className="footer-brand1">
+          <span className="footer-brand1">
+            © 2023 <span className="brand-color1">FarmaTurno</span>.
+          </span>{' '}
+          Todos los derechos reservados
+        </p>
+        <Row lg={3} className="footer-links1 justify-content-center">
+          <Col xs={12}>
+            <a href="/privacidad" className="footer-link1 p-0">
+              Políticas de privacidad
+            </a>
+          </Col>
+          <Col xs={12}>
+            <a href="/terminos" className="footer-link1 p-0">
+              Términos y condiciones
+            </a>
+          </Col>
         </Row>
-      </div>
+      </Row>
     </footer>
   );
 };
